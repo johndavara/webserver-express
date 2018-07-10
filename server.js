d889d4c3ec6d27f8d0788de8705dffb5d34e7e2f
Johnnatan Vargas Ramirez <johnnatan.vargas.ti.developer@gmail.com>
2018-07-10 15:24:21 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+//Express HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+/*
+app.get('/', function(req, res) {
+    //res.send('Hola Mundo')
+    let salida = {
+        nombre: 'Johnnatan',
+        edad: 25,
+        url: req.url
+    };
+    res.send(salida);
+})
+*/
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'Johnnatan',
+    })
+})
+
+app.get('/about', function(req, res) {
+    res.render('about', {
+        imagen: 'assets/img/ranarene.jpg',
+        h1: "La Rana Rene",
+    })
+})
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
+
+//npm run nodemon
